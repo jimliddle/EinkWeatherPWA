bc9811d03f13341eff6b0792f1609b82f5534291
jimliddle <jimliddle@icloud.com>
2026-01-23 17:48:25 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'e-ink-weather-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching offline assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event (Cache First, then Network)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached version if found, otherwise fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event (Cleanup old caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
